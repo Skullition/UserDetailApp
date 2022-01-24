f2ed570d2638d2d00ec454d15c3e3aa75f033aab
Skullition <78258797+Skullition@users.noreply.github.com>
2022-01-24 17:39:26 +0700

    Redesign homepage to use cards

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import {
   FlatList,
   Image,
   SafeAreaView,
-  StatusBar,
   StyleSheet,
   Text,
   View,
@@ -26,7 +25,7 @@ const App = () => {
   }, []);
 
   return (
-    <View style={{flex: 1, padding: 24}}>
+    <View>
       {isLoading ? (
         <ActivityIndicator />
       ) : (
@@ -38,13 +37,18 @@ const App = () => {
           renderItem={({item}) => {
             return (
               <SafeAreaView style={styles.container}>
-                <Image
-                  style={styles.image}
-                  source={{
-                    uri: item.avatar_url,
-                  }}
-                />
-                <Text style={styles.item}>{item.login}</Text>
+                <View style={styles.card_template}>
+                  <Image
+                    style={styles.card_image}
+                    source={{
+                      // @ts-ignore
+                      uri: item.avatar_url,
+                    }}
+                  />
+                  <View style={styles.text_container}>
+                    <Text style={styles.card_title}>{item.login}</Text>
+                  </View>
+                </View>
               </SafeAreaView>
             );
           }}
@@ -56,18 +60,31 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  item: {
-    backgroundColor: '#554757',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+  card_template: {
+    marginTop: 35,
+    boxShadow: '10px 10px 17px -12px rgba(0,0,0,0.75)',
   },
-  title: {
-    fontSize: 32,
+  card_image: {
+    width: 250,
+    height: 250,
+    borderRadius: 10,
+  },
+  text_container: {
+    position: 'absolute',
+    width: 250,
+    height: 30,
+    bottom: 0,
+    padding: 5,
+    backgroundColor: 'rgba(0,0,0, 0.3)',
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  card_title: {
+    color: 'white',
   },
   image: {
     width: 60,
